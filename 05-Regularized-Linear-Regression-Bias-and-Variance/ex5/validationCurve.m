aba855b7995d5ce19c06a1ede1fd7b922143fe51
Josh Terrell <joshterrell805@gmail.com>
2015-08-31 08:00:26 -0700

    05: validation curve (train/val errors as change lambda)

--- 05-Regularized-Linear-Regression-Bias-and-Variance/ex5/validationCurve.m
+++ 05-Regularized-Linear-Regression-Bias-and-Variance/ex5/validationCurve.m
@@ -36,17 +36,13 @@ error_val = zeros(length(lambda_vec), 1);
 %           ....
 %           
 %       end
-%
-%
 
-
-
-
-
-
-
-
-
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X, y, lambda);
+  error_train(i) = linearRegCostFunction(X, y, theta, 0)(1);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0)(1);
+end
 
 % =========================================================================
 
